ddfe01e62fd47d9eb54bf2556c2945df4794826e
prashant333 <pmani157@gmail.com>
2019-01-14 23:34:41 +0530

    updated  answer 2

--- chapter4.js
+++ chapter4.js
@@ -1,31 +1,31 @@
-// // question 1: The sum of a range
-// function rangearray(a,b,c){
-//     var arr=[]
-//     if (b>a) {
-//         while (b>=a) {
-//             arr.push(a)
-//             a+=c
-//         }
-//     }
-//     else if (a>b) {
-//         while (a>=b) {
-//             arr.push(a)
-//             a-=c
-//         }
-//     } else {
-//         console.log("enter valid range")   
-//     }
-//     sum(arr)    
-//     console.log(arr)
-// }
-// function sum(array) {
-//     var total=0
-//     for (let index = 0; index < array.length; index++) {
-//         total += array[index];   
-//     }
-//     console.log(total)
-// }
-// rangearray(1,10,2)
+// question 1: The sum of a range
+function rangearray(a,b,c){
+    var arr=[]
+    if (b>a) {
+        while (b>=a) {
+            arr.push(a)
+            a+=c
+        }
+    }
+    else if (a>b) {
+        while (a>=b) {
+            arr.push(a)
+            a-=c
+        }
+    } else {
+        console.log("enter valid range")   
+    }
+    sum(arr)    
+    console.log(arr)
+}
+function sum(array) {
+    var total=0
+    for (let index = 0; index < array.length; index++) {
+        total += array[index];   
+    }
+    console.log(total)
+}
+rangearray(1,10,2)
 
 // question 2: function to reverse an array
 // function reverseArray(arr) {
