bd72c70a97aaa21fef838f1a94cf608356ff9ffa
prashant333 <pmani157@gmail.com>
2019-01-14 17:38:29 +0530

    step parameter added

--- chapter4.js
+++ chapter4.js
@@ -1,27 +1,28 @@
 // question 1: The sum of a range
-function rangearray(a,b){
+function rangearray(a,b,c){
     var arr=[]
     if (b>a) {
         while (b>=a) {
             arr.push(a)
-            a++
+            a+=c
         }
     }
     else if (a>b) {
         while (a>=b) {
             arr.push(a)
-            a--
+            a-=c
         }
     } else {
         console.log("enter valid range")   
     }
-    sum(arr)    
+    // sum(arr)    
+    console.log(arr)
 }
-function sum(array) {
-    var total=0
-    for (let index = 0; index < array.length; index++) {
-        total += array[index];   
-    }
-    console.log(total)
-}
-rangearray(1,10)
+// function sum(array) {
+//     var total=0
+//     for (let index = 0; index < array.length; index++) {
+//         total += array[index];   
+//     }
+//     console.log(total)
+// }
+rangearray(1,10,2)
