6b88068dae9fc7bc4b452913a0d2a75f4430f625
prashant333 <pmani157@gmail.com>
2019-01-21 08:53:48 +0530

    solution 2 modified

--- chapter3.js
+++ chapter3.js
@@ -5,16 +5,16 @@ function min(a,b){
 }
 console.log(min(0,10))
 console.log(min(0,-10))
+
 //  question 2 RECURSION
 function isEven(n) {
-    if(n==0)
-        console.log(`the number ${n} is even`)
-    else if (n==1) {
-        console.log(`the number ${n} is odd`)
-    } else {
-        isEven(n-2)
-    }
+    if (n==0) return true;
+    else if(n==1) return false;
+    else if(n<0) return isEven(-n)
+    else return isEven(n-2);
     
 }
-isEven(-1)
+console.log(isEven(50))
+console.log(isEven(75))
+console.log(isEven(-1))
 
