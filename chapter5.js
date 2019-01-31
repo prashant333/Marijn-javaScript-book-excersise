6565f3c21a988a2dad69d2335cec29478f912c95
prashant333 <pmani157@gmail.com>
2019-01-31 11:06:37 +0530

    improved flatten example

--- chapter5.js
+++ chapter5.js
@@ -1,13 +1,17 @@
-// flatten array value.
-let arr = [[23,34],[20,28],[78,87]]
-let xyz = []
-flatten(arr)
-function flatten(arrayValue) {
-    for(let x of arrayValue){
-        for(let y of x){
-            xyz.push(y)
-        }
-    }
+// // flatten array value.
+// let arr = [[23,34],[20,28],[78,87]]
+// let xyz = []
+// flatten(arr)
+// function flatten(arrayValue) {
+//     for(let x of arrayValue){
+//         for(let y of x){
+//             xyz.push(y)
+//         }
+//     }
 
-    console.log(xyz)
-}
+//     console.log(xyz)
+// }
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce((flat, current) => flat.concat(current), []));
