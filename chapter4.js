6955b9afc5231487b253e4a81c64b07ccadf67a1
Prashant Mani <pmani157@gmail.com>
2019-01-31 22:56:17 +0530

    list question

--- chapter4.js
+++ chapter4.js
@@ -46,13 +46,50 @@ rangearray(1,10,2)
 // }
 // reverseArray([1,2,3,4,5])
 
-function reverseArr(input) {
-    var ret = new Array;
-    for(var i = input.length-1; i >= 0; i--) {
-        ret.push(input[i]);
-    }
-    console.log(ret)
-}
+// function reverseArr(input) {
+//     var ret = new Array;
+//     for(var i = input.length-1; i >= 0; i--) {
+//         ret.push(input[i]);
+//     }
+//     console.log(ret)
+// }
 
-var a = [3,5,7,8]
-var b = reverseArr(a);
+// var a = [3,5,7,8]
+// var b = reverseArr(a);
+
+//  A list
+
+function arrayToList(array) {
+    let list = null;
+    for (let i = array.length - 1; i >= 0; i--) {
+      list = {value: array[i], rest: list};
+    }
+    return list;
+  }
+  
+  function listToArray(list) {
+    let array = [];
+    for (let node = list; node; node = node.rest) {
+      array.push(node.value);
+    }
+    return array;
+  }
+  
+  function prepend(value, list) {
+    return {value, rest: list};
+  }
+  
+  function nth(list, n) {
+    if (!list) return undefined;
+    else if (n == 0) return list.value;
+    else return nth(list.rest, n - 1);
+  }
+  
+  console.log(arrayToList([10, 20]));
+  // → {value: 10, rest: {value: 20, rest: null}}
+  console.log(listToArray(arrayToList([10, 20, 30])));
+  // → [10, 20, 30]
+  console.log(prepend(10, prepend(20, null)));
+  // → {value: 10, rest: {value: 20, rest: null}}
+  console.log(nth(arrayToList([10, 20, 30]), 1));
+  // → 20
